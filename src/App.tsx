e25c9d38cb06b9b39408dfd9147f82802334ab2c
BalintCsala <balintcsala@gmail.com>
2023-11-22 06:58:19 +0100

    Drag and drop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SupportButton from "./SupportButton";
 
 export default function App() {
     const [file, setFile] = createSignal<File | null>(null);
+    const [fileOver, setFileOver] = createSignal(false);
 
     const fileUpload = (
         <input
@@ -27,23 +28,44 @@ export default function App() {
                     <AboutButton />
                 </div>
             </header>
-            <main class="relative flex w-full grow flex-col overflow-y-hidden border-2 border-primary-600">
+            <main
+                onDrop={e => {
+                    e.preventDefault();
+                    const item = e.dataTransfer?.items[0];
+                    if (!item || item.kind !== "file") return;
+
+                    setFile(item.getAsFile());
+                }}
+                onDragOver={e => {
+                    setFileOver(true);
+                    e.preventDefault();
+                }}
+                onDragLeave={() => setFileOver(false)}
+                class="relative flex w-full grow flex-col overflow-y-hidden border-2 border-primary-600">
                 <Show
                     when={file()}
                     fallback={
                         <div class="flex h-full w-full items-center justify-center">
-                            <button
-                                onClick={() => fileUpload.click()}
-                                class="flex flex-col items-center border-4 border-dotted border-primary-600 bg-primary-950 p-8 transition-colors hover:bg-primary-900">
-                                <Icon
-                                    icon="upload"
-                                    class="text-9xl text-primary-400"
-                                />
-                                <span class="text-primary-400">
-                                    Select a shader
-                                </span>
-                                {fileUpload}
-                            </button>
+                            <Show
+                                when={!fileOver()}
+                                fallback={
+                                    <div class="pointer-events-none text-primary-400">
+                                        Release to upload
+                                    </div>
+                                }>
+                                <button
+                                    onClick={() => fileUpload.click()}
+                                    class="flex flex-col items-center border-4 border-dotted border-primary-600 bg-primary-950 p-8 transition-colors hover:bg-primary-900">
+                                    <Icon
+                                        icon="upload"
+                                        class="text-9xl text-primary-400"
+                                    />
+                                    <span class="text-primary-400">
+                                        Select a shader
+                                    </span>
+                                    {fileUpload}
+                                </button>
+                            </Show>
                         </div>
                     }>
                     {file => <ShaderEditor file={file()} />}
