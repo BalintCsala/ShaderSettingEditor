824e475067957995f6622daee0409ebbd1d45520
BalintCsala <balintcsala@gmail.com>
2023-11-22 07:24:15 +0100

    Better color button text

--- src/editor/screen/elements/ColorButton.tsx
+++ src/editor/screen/elements/ColorButton.tsx
@@ -83,7 +83,7 @@ export default function ColorButton(props: Props) {
                     }}
                 />
             </Show>
-            <span class="text-shadow pointer-events-none select-none text-black shadow-red-600 drop-shadow-lg">
+            <span class="text-stroke pointer-events-none select-none font-bold text-black shadow-red-600 drop-shadow-lg">
                 {props.lang.option[props.colorChanger.name]?.text ||
                     props.colorChanger.name}
             </span>
