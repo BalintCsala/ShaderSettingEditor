bbf5219a210566def6e185817abe6665e4b25508
BalintCsala <balintcsala@gmail.com>
2023-11-30 06:11:28 +0100

    Fix

--- src/getProjectVersions.ts
+++
@@ -1,5 +0,0 @@
-async function fetchProjectVersions(slug: string) {
-    const response = await fetch(API_URL + "project/" + slug + "/version");
-    const data = (await response.json()) as ProjectVersionsResponse;
-    return data;
-}
