e2c8b22793a0f685a95320b1f2508125365a3863
BalintCsala <balintcsala@gmail.com>
2024-01-07 02:36:03 +0100

    Auth base

---
+++ src/Auth.tsx
@@ -0,0 +1,14 @@
+export default function Auth() {
+    return (
+        <main class="flex h-screen w-screen items-center justify-center">
+            <div class="border-2 border-primary-600 p-12 text-center">
+                <h1 class="mb-4 text-3xl text-primary-400">
+                    Successfully signed in!
+                </h1>
+                <span class="text-primary-400">
+                    This window will automatically close in 3 seconds...
+                </span>
+            </div>
+        </main>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import "./index.css";
 
 /* @refresh reload */
+import { Route, Router } from "@solidjs/router";
 import { render } from "solid-js/web";
 import App from "./App";
+import Auth from "./Auth";
 
 const root = document.getElementById("root");
 
-render(() => <App />, root!);
+render(
+    () => (
+        <Router>
+            <Route path="/" component={App} />
+            <Route path="/auth/callback" component={Auth} />
+        </Router>
+    ),
+    root!,
+);
